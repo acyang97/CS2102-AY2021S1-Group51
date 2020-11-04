fcf81f257c75e87761582346fd25c41151dfdce9
Chun Yang <acyang97@gmail.com>
2020-11-04 11:07:00 +0800

    transferred automation of changes to salary table trigger from read_data.sql to here

--- init.sql
+++ init.sql
@@ -336,6 +336,41 @@ CREATE TRIGGER update_caretaker_rating_after_petowner_give_rating_trigger
   FOR EACH ROW
   EXECUTE PROCEDURE update_caretaker_rating_after_petowner_give_rating_function();
 
+CREATE OR REPLACE FUNCTION update_salary() RETURNS trigger AS $$
+  DECLARE
+    end_of_month date := (SELECT(date_trunc('month', NEW.start_date::date) + interval '1 month' - interval '1 day')::date);
+  DECLARE
+    start_of_end_date_month date;
+  BEGIN
+  IF NEW.end_date > end_of_month THEN
+  start_of_end_date_month := (SELECT date_trunc('MONTH', NEW.end_date)::DATE);
+
+  UPDATE CareTakerSalary
+  SET earnings = earnings + ((NEW.end_date - start_of_end_date_month + 1) * NEW.price), petdays = petdays + (NEW.end_date - start_of_end_date_month + 1)
+  WHERE
+  username = NEW.ct_username AND
+  year = (SELECT date_part('year', start_of_end_date_month)) AND
+  month = (SELECT date_part('month', start_of_end_date_month));
+
+  NEW.end_date := end_of_month;
+  END IF;
+  UPDATE CareTakerSalary
+  SET earnings = earnings + ((NEW.end_date - NEW.start_date + 1) * NEW.price), petdays = petdays + (NEW.end_date - NEW.start_date + 1)
+  WHERE
+  username = NEW.ct_username AND
+  year = (SELECT date_part('year', NEW.start_date)) AND
+  month = (SELECT date_part('month', NEW.start_date));
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_ct_salary
+  AFTER INSERT
+  ON "bids"
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_salary();
+
 -- NEED HELP FOR THIS!!
 -- procedure that is excuted by the trigger below
 --CREATE OR REPLACE FUNCTION check_if_fulltime_can_take_leave_function()  RETURNS trigger AS $$
