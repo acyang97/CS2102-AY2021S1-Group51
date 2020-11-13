f01a535b506cff9ee5154b2c0b3c7756b642f3dd
ivanlim <ivanlim97@hotmail.com>
2020-11-13 22:58:50 +0800

    try to fix port error

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -43,8 +43,7 @@ login_manager.init_app(app)
 #        #port=5000
 #        port = int(os.environ.get("PORT", 5000))
 #    )
-port = int(os.environ.get('PORT', 33507))
 
 if __name__ == "__main__":
-    app.debug = True
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
