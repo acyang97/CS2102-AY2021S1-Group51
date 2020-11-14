f4bdd4c3143e661aad470ea088d33713d620f77f
acyang97 <acyang97@gmail.com>
2020-11-14 23:21:38 +0800

    handle ui

--- forms.py
+++ forms.py
@@ -213,7 +213,7 @@ class searchsalaryForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class AdminViewUnderperformingCareTakerForm(FlaskForm):
-    years = [('2020', '2020'), ('2021', '2021')]
+    years = [('2018', '2018'), ('2019', '2019'), ('2020', '2020'), ('2021', '2021'), ('2022', '2022')]
     months = [('1', 'January'), ('2','February'), ('3', 'March'), ('4','April'), ('5', 'May'), ('6', 'June'),
                 ('7', 'July'), ('8', 'August'), ('9', 'September'), ('10', 'October'), ('11', 'November'), ('12', 'December')]
     year = SelectField('Year', choices = years)
